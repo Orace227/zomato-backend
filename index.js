83d6940466d92a29ea42f4d66d04560b7ee4c582
Orbit_Gamerz <gklibrary2020@gmail.com>
2023-08-17 13:51:36 +0530

    edit api done and solve some bugs in backend

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const auth = require("./middleware/auth");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-
+const axios = require("axios");
 // here all varables are defined
 const port = 5000;
 const jwtSecret = process.env.JWT_SECRET_KEY;
@@ -52,26 +52,32 @@ app.get("/profile", (req, res) => {
 app.post("/signup", async (req, res) => {
   try {
     const { username, email, password } = req.body;
-    const createdUser = await User.create({ username, email, password });
-    const fetchedUser = await User.findOne({ email, password }).select(
-      "-password"
-    );
-    jwt.sign(
-      { userid: createdUser._id, username },
-      jwtSecret,
-      {},
-      (err, token) => {
-        res
-          .cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none",
-          })
-          .status(201)
-          .json(fetchedUser);
-        if (err) throw err;
-      }
-    );
+    const existedUser = await User.findOne({ email });
+    if (existedUser) {
+      res.json("user exist");
+    }
+    if (!existedUser) {
+      const createdUser = await User.create({ username, email, password });
+      const fetchedUser = await User.findOne({ email }).select("-password");
+
+      jwt.sign(
+        { userid: createdUser._id, username },
+        jwtSecret,
+        {},
+        (err, token) => {
+          res
+            .cookie("token", token, {
+              httpOnly: true,
+              secure: true,
+              sameSite: "none",
+              expires: new Date(Date.now() + 900000000),
+            })
+            .status(201)
+            .json(fetchedUser);
+          if (err) throw err;
+        }
+      );
+    }
   } catch (err) {
     if (err) throw err;
     res.status(500).json("error");
@@ -81,7 +87,7 @@ app.post("/signup", async (req, res) => {
 app.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    // console.log(email, password);
+    const expirationTime = 15 * 24 * 60 * 60 * 1000;
 
     const checkedUser = await User.findOne({ email, password }).select(
       "-password"
@@ -97,6 +103,7 @@ app.post("/login", async (req, res) => {
               httpOnly: true,
               secure: true,
               sameSite: "none",
+              expires: new Date(Date.now() + expirationTime),
             })
             .status(201)
             .json(checkedUser);
@@ -118,6 +125,89 @@ app.get("/logout", async (req, res) => {
     res.status(500).json(err.message);
   }
 });
+
+////////////////////// edit profile //////////////////////
+
+app.post("/editProfile", async (req, res) => {
+  try {
+    const { id, username, email, password } = req.body;
+    const existedUser = await User.findOne({ email });
+    if (!existedUser) {
+      console.log("user does not exist");
+      const isEdited = await User.updateOne(
+        { _id: id },
+        { $set: { username, email, password } }
+      );
+
+      const editedUser = await User.findOne({ email }).select("-password");
+
+      jwt.sign(
+        { userid: editedUser.id, username: editedUser.username },
+        jwtSecret,
+        {},
+        (err, token) => {
+          res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            expires: new Date(Date.now() + expirationTime),
+          });
+          if (err) throw err;
+        }
+      );
+      res.status(200).json(editedUser);
+    }
+    if (existedUser) {
+      if (existedUser._id == id) {
+        const isEdited = await User.updateOne(
+          { _id: id },
+          { $set: { username, email, password } }
+        );
+
+        const editedUser = await User.findOne({ email }).select("-password");
+
+        jwt.sign(
+          { userid: editedUser.id, username: editedUser.username },
+          jwtSecret,
+          {},
+          (err, token) => {
+            res.cookie("token", token, {
+              httpOnly: true,
+              secure: true,
+              sameSite: "none",
+              expires: new Date(Date.now() + expirationTime),
+            });
+            if (err) throw err;
+          }
+        );
+        res.status(200).json(editedUser);
+      } else {
+        res.status(409).json("user exist");
+      }
+    }
+  } catch (err) {
+    throw err;
+  }
+});
+
+//////////////////////get restaurant ////////////////////////
+// app.get("/fetch-swiggy-data", async (req, res) => {
+//   try {
+//     const { lat, lng } = req.query;
+//     console.log({ lat, lng });
+//     const swiggyApiUrl = `https://www.swiggy.com/dapi/restaurants/list/v5?lat=${lat}&lng=${lng}`;
+//     const headers = {
+//       "Content-Type": "application/json",
+//     };
+//     const response = await axios.get(swiggyApiUrl, { headers });
+
+//     // res.json(response.data);
+//   } catch (error) {
+//     console.log(error);
+//     // res.json(error);
+//   }
+// });
+//////////////////////server erea ////////////////////////
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
