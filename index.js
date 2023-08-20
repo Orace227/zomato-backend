f6e8c917d311433775a28b9406434df7ee9e17ce
Orbit_Gamerz <gklibrary2020@gmail.com>
2023-08-20 11:57:12 +0530

    added url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "UPDATE", "DELETE"],
     credentials: true,
   })
